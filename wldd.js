2a88940650631521545f07fe85ad94ed6a2ba402
Akash Kurdekar <akashkurdekar39@gmail.com>
2024-07-12 19:37:28 +0530

    .

---
+++ wldd.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+  const navList = document.getElementById("navList");
+  const burger = document.getElementById("burger");
+  navList.classList.toggle("active");
+  burger.classList.toggle("active");
+}
+
+// Close menu when a link is clicked
+const navLinks = document.querySelectorAll(".nav-items a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    const navList = document.getElementById("navList");
+    navList.classList.remove("active");
+  });
+});
